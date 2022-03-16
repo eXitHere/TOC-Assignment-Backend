cf6715651f896e4722f92178b18b07c48e4b93d7
It5Me <56817602+It5Me@users.noreply.github.com>
2022-03-16 23:15:16 +0700

    CREATE: ReacfileFromUser

--- src/index.js
+++ src/index.js
@@ -29,7 +29,6 @@ app.use('/api', routes);
 const upload = multer();
 app.post('/upload', upload.single('file'), function (req, res) {
   console.log(req.file, req.body);
-
   console.log('req.file.buffer', req.file.buffer.toString());
   res.status(200).send(req.file.buffer.toString());
 });
