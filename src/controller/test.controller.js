4032652eef588948ea8555ffa97c6501f0499b36
It5Me <56817602+It5Me@users.noreply.github.com>
2022-03-16 23:13:35 +0700

    CREATE: ReacfileFromUser

--- src/controller/test.controller.js
+++ src/controller/test.controller.js
@@ -1,11 +1,15 @@
 const helloWorld = require('../utils/test');
 const callScript = require('../utils/callScript');
 const { getValue } = require('../utils/caching');
-
+const multer = require('multer');
 exports.test = async (req, res) => {
   res.status(200).send(await getValue());
 };
 
 exports.testScript = async (req, res) => {
   res.status(200).send({ jsonResponse: await callScript.test() });
+};
+exports.testUpload = async (req, res) => {
+  console.log(req.files);
+  res.status(200).send({ jsonResponse: 'hello' });
 };
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const testController = require('./controller/test.controller');
 
 Router.get('/', testController.test);
 Router.get('/test', testController.testScript);
+// Router.post('/upload', testController.testUpload);
 // /api/subject/:type
 // วิชาภาค
 // วิชาเลือก
